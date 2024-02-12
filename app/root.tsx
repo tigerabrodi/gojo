6ad4f45adbf6aec0d9358c827e88a906549d43e7
Tiger Abrodi <tigerabrodi@gmail.com>
2024-02-12 05:25:47 +0100

    Initial commit from create-remix

---
+++ app/root.tsx
@@ -0,0 +1,35 @@
+import { cssBundleHref } from "@remix-run/css-bundle";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+import { Analytics } from "@vercel/analytics/react";
+import type { LinksFunction } from "@vercel/remix";
+
+export const links: LinksFunction = () => [
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+];
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+        <Analytics />
+      </body>
+    </html>
+  );
+}
