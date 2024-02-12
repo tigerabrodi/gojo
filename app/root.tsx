207e42522292f9a461ef094c9703728c834fd460
Tiger Abrodi <tigerabrodi@gmail.com>
2024-02-12 06:26:49 +0100

    Add root.css and update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -8,18 +8,38 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import { Analytics } from "@vercel/analytics/react";
-import type { LinksFunction, MetaFunction } from "@vercel/remix";
+import rootStyles from "./styles/root.css";
+import {
+  redirect,
+  type LinksFunction,
+  type LoaderFunctionArgs,
+  type MetaFunction,
+} from "@vercel/remix";
+import { getAuthFromRequest } from "./auth/auth";
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+  { rel: "stylesheet", href: rootStyles },
 ];
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Gojo" },
+    {
+      name: "description",
+      content:
+        "Gojo is a site where you can brainstorm ideas with friends in real-time.",
+    },
   ];
 };
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  let userId = await getAuthFromRequest(request);
+  if (userId && new URL(request.url).pathname === "/") {
+    throw redirect("/login");
+  }
+  return userId;
+}
 
 export default function App() {
   return (
