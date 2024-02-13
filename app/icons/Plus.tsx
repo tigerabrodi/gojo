319d17d6c6414c907a31c0d9396200ea2bd7b457
Tiger Abrodi <tigerabrodi@gmail.com>
2024-02-13 06:53:16 +0100

    Add Plus icon and create board button to Boards route

---
+++ app/icons/Plus.tsx
@@ -0,0 +1,10 @@
+export function Plus() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="none">
+      <path
+        fill="#0F0F0F"
+        d="M17.6 1.6a1.6 1.6 0 1 0-3.2 0v12.8H1.6a1.6 1.6 0 1 0 0 3.2h12.8v12.8a1.6 1.6 0 1 0 3.2 0V17.6h12.8a1.6 1.6 0 1 0 0-3.2H17.6V1.6Z"
+      />
+    </svg>
+  );
+}
--- app/routes/boards/route.tsx
+++ app/routes/boards/route.tsx
@@ -1,3 +1,36 @@
+import { Form, Link } from "@remix-run/react";
+import { FORM_INTENTS, INTENT } from "~/helpers";
+import { Plus } from "~/icons";
+
+import styles from "./styles.css";
+import type { LinksFunction } from "@vercel/remix";
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+
 export default function Boards() {
-  return <div>hello world</div>;
+  return (
+    <main>
+      <div>
+        <h1>Boards</h1>
+        <ul>
+          <li>
+            <Link to="/boards/1">
+              <span className="name">Untitled</span>
+              <span className="date">Last opened: 2021-10-10</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <Form method="post">
+        <button
+          aria-label="Create board"
+          name={INTENT}
+          value={FORM_INTENTS.createBoard}
+        >
+          <Plus />
+        </button>
+      </Form>
+    </main>
+  );
 }
