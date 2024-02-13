6dccb53dc787573a85c76c964c3759e14ce63556
Tiger Abrodi <tigerabrodi@gmail.com>
2024-02-13 08:48:46 +0100

    Add new board route and styles

---
+++ app/routes/boards_.$id/route.tsx
@@ -0,0 +1,54 @@
+import type { LinksFunction, LoaderFunctionArgs } from "@vercel/remix";
+import { json } from "@vercel/remix";
+import { requireAuthCookie } from "~/auth";
+import { invariant } from "@epic-web/invariant";
+import { useLoaderData } from "@remix-run/react";
+import { RoomProvider } from "~/liveblocks.config";
+import { LiveList } from "@liveblocks/client";
+import { ClientSideSuspense } from "@liveblocks/react";
+import styles from "./styles.css";
+import { Kakashi } from "~/icons";
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+
+export async function loader({ params, request }: LoaderFunctionArgs) {
+  await requireAuthCookie(request);
+
+  const boardId = params.id;
+
+  invariant(boardId, "No board ID provided");
+
+  return json({
+    boardId,
+  });
+}
+
+function SuspenseFallback() {
+  return (
+    <main>
+      <Kakashi className="suspense-fallback-loader" />
+    </main>
+  );
+}
+
+export default function BoardRoute() {
+  const { boardId } = useLoaderData<typeof loader>();
+
+  return (
+    <RoomProvider
+      id={boardId}
+      initialPresence={{ cursor: null }}
+      initialStorage={{
+        cards: new LiveList(),
+      }}
+    >
+      <ClientSideSuspense fallback={<SuspenseFallback />}>
+        {() => <SuspenseFallback />}
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+}
+
+function Board() {
+  return <div>Board</div>;
+}
