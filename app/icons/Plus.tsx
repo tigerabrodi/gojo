3459b5b482b97c1dbf6e6072096b246af56d514f
Tiger Abrodi <tigerabrodi@gmail.com>
2024-02-17 08:47:03 +0100

    fix: styling update to boards page

--- app/icons/Plus.tsx
+++ app/icons/Plus.tsx
@@ -1,9 +1,9 @@
 export function Plus() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="none">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 28 28">
       <path
         fill="#0F0F0F"
-        d="M17.6 1.6a1.6 1.6 0 1 0-3.2 0v12.8H1.6a1.6 1.6 0 1 0 0 3.2h12.8v12.8a1.6 1.6 0 1 0 3.2 0V17.6h12.8a1.6 1.6 0 1 0 0-3.2H17.6V1.6Z"
+        d="M15.4 1.4a1.4 1.4 0 0 0-2.8 0v11.2H1.4a1.4 1.4 0 0 0 0 2.8h11.2v11.2a1.4 1.4 0 1 0 2.8 0V15.4h11.2a1.4 1.4 0 1 0 0-2.8H15.4V1.4Z"
       />
     </svg>
   );
